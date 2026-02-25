78700d3f0903e5053a8b518dd2642efc09ed5f6b
fuchse-ohren <git@kitsune.aleeas.com>
2026-02-26 00:46:59 +0900

    外観を変更

--- settings/script.js
+++ settings/script.js
@@ -1,9 +1,21 @@
-function sv(){
-	var message = document.getElementById('message');
+function clear(){
+		//すべてのメッセージを消す
+	document.getElementById("regex").ariaInvalid = false;
+	document.getElementById("excregex").ariaInvalid = false;
+	document.getElementById('regex-error-text').style.display = "none";
+	document.getElementById('excregex-error-text').style.display = "none";
+	document.getElementById("message").innerText = "";
+}
 
+function sv(){
+	
+	//すべてのメッセージを消す
+	clear();
+	
 	if(document.getElementById("regex").value === ""){
-		message.innerText = "何か入力したらどうですか？";
-		message.style.color="#e78B90";
+		document.getElementById('regex-error-text').innerText = "＊この項目は入力必須です";
+		document.getElementById('regex-error-text').style.display = "";
+		document.getElementById("regex").ariaInvalid = true;
 		return;
 	}
 	
@@ -12,18 +24,16 @@ function sv(){
 		var thisRegex = new RegExp(document.getElementById("regex").value);
 		localStorage.setItem("targetURL", document.getElementById("regex").value);
 		console.log(localStorage.getItem("targetURL"));
-		message.innerText = "登録しました";
-		message.style.color="#e78B90";
 	}catch{
-		message.innerText = "対象URLの正規表現が正しくありません";
-		message.style.color="#e78B90";
+		document.getElementById('regex-error-text').innerText = "＊正規表現の書式が誤っています";
+		document.getElementById('regex-error-text').style.display = "";
+		document.getElementById("regex").ariaInvalid = true;
+		return;
 	}
 	
 	//除外URLの登録
-		if(document.getElementById("excregex").value === ""){
-			message.innerText = "何か入力したらどうですか？";
-			message.style.color="#e78B90";
-			return;
+	if(document.getElementById("excregex").value === ""){
+		document.getElementById("excregex").value = "^$"
 	}
 	
 	try{
@@ -31,20 +41,29 @@ function sv(){
 		localStorage.setItem("exclusionURL", document.getElementById("excregex").value);
 		console.log(localStorage.getItem("exclusionURL"));
 	}catch{
-		message.innerText = "除外URLの正規表現が正しくありません";
-		message.style.color="#e78B90";
+		document.getElementById('excregex-error-text').innerText = "＊正規表現の書式が誤っています";
+		document.getElementById("excregex").ariaInvalid = true;
+		document.getElementById('excregex-error-text').style.display = "";
+		return;
 	}
+	
+	document.getElementById("message").innerText = "登録しました";
 }
 
 function rm(){
+	//すべてのメッセージを消す
+	clear();
+	
 	const message = document.getElementById("log_message");
 	localStorage.setItem("log","{}");
 	message.innerText = "ログを消去しました";
-	message.style.color="#e78B90";
 	document.getElementById("logsize").innerText = String(localStorage.getItem("log").length)+" バイト";
 }
 
 function dl(){
+	//すべてのメッセージを消す
+	clear();
+	
 	const log = localStorage.getItem("log");
 	const blob = new Blob([log], { type: 'application/json' });
 	let a = document.createElement('a');
@@ -56,10 +75,12 @@ function dl(){
 }
 
 function reset(){
+	//すべてのメッセージを消す
+	clear();
+	
 	localStorage.setItem("targetURL", 'htt(p|ps)://example.net($|/.*)');
 	localStorage.setItem("exclusionURL", '(.*\\.)(jpg|jpeg|png|gif|svg|ico|css|js|woff|woff2|mp4|mp3|ts|m3u8|pdf|doc|docx|ppt|pptx|xls|xlsx|md)$');
 	message.innerText = "設定をリセットしました";
-	message.style.color="#e78B90";
 	document.getElementById("regex").value = localStorage.getItem("targetURL");
 	document.getElementById("excregex").value = localStorage.getItem("exclusionURL");
 	document.getElementById("logsize").innerText = String(localStorage.getItem("log").length)+" バイト";
